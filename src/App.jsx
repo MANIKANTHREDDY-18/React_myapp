a108523b7c8375215dad315a869323c3d32bd0bb
manikanth <kanalamanikanthreddy18@gmail.com>
2026-04-29 10:01:39 +0530

    feat :added About,Projects,Skills,Home,Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Home from "./components/Home"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
 
 function App() {
 
   return (
-   <h1>hello</h1>
+    <>
+      <h1>hello</h1>
+      <h2>bye</h2>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+      
+    </>
   )
 }
 
